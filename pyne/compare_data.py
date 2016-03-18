934c43631cde596942e2f1923a04b8e05e9b1022
qingquan <qwang252@wisc.edu>
2016-03-17 19:01:30 -0500

    revise compare_data.py to unit comparsion

--- pyne/compare_data.py
+++ pyne/compare_data.py
@@ -1,37 +1,36 @@
-"""this method compare two columns of results and errors, return
-   ture if the two columns have the same results otherwise return
-   false. Also return the list of absolute difference btw these 
-   two columns"""
-def compare(mode,file1,file2):
-    return_type = True
-    diff = []
-    lines1 = file1.readlines()
-    lines2 = file2.readlines()
-    # assume the size  of file1 and file2 are the same
-    for i in range(0,len(lines1)):
-          list1 = lines1[i].strip().split()
-          list1 = [float(item) for item in list1]
-          list2 = lines2[i].strip().split()      
-          list2 = [float(item) for item in list2]
-    # compare the results only
-          if (mode==1):  
-            if(list1[0]!=list2[0]): 
-              return_type = False
-          
-    # compare result1 and error1 with result2     
-          if mode==2: 
-            if list1[0]+list1[1] >=  list2[0] >= list1[1]-list1[0]:
-               pass
-            else: return_type=False
-     
-    
-     
-          if mode == 3:
-               pass   
-         
-          diff.append(abs(list1[0]-list2[0]))
-
-    return return_type, diff
-
-
-
+def compare_data(mode, val1, err1, val2, err2):     
+   return_type= True
+   diff= 0
+   try:
+      mode=int(mode) 
+      val1=float(val1)
+      val2=float(val2)
+      err1=float(err1)
+      err2=float(err2)
+   except ValueError:
+      print("that was not valid number..")
+      return 
+   if mode ==1:
+       #compare val1 and val2
+       if val1!=val2:
+          return_type= False
+   elif mode ==2:
+       #compare val2 with val1 and err1
+       if val1+err1 >= val2 >= val1-err1:
+          pass
+       else:
+          return_type=False
+   elif mode ==3:
+       #compare val1 with val2 and err2
+       if val2+err2 >= val1 >= val2-err2:
+          pass
+       else:
+          return_type=False
+   else: 
+         print("error: mode type is not supported. ")
+         return
+  
+   diff=abs(val1-val2)
+   return return_type, diff
+             
+       
