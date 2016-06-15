cf5f4a43ec0f0288c94555315f170cc46139c66f
qingquan <qwang252@wisc.edu>
2016-06-15 16:07:38 -0500

    add tally

--- pyne/python_opener.py
+++ pyne/python_opener.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
-from pyne.mcnp import Mctal
+from pyne.mcnp import Mctal 
+from pyne.mcnp import Tally
 data = Mctal()
-print (data.read("mctal"))
+tally = Tally()
+
+data.read("mctal")
+tally.read("mctal")
